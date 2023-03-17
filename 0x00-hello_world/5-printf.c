e3a21e11a45af0db4b3631976770d7c4ea397bc9
moemn10 <moemnkoko@gmail.com>
2023-03-16 18:40:04 -0700

    Moamen

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
-* main -Entry point 
-* Description: A C program that prints whith printf function.
-*
-* Return: Always 0 (Success)
+ * main -Entry point
+ *
+ * Description: A C program that prints whith printf function.
+ *
+ * Return: Always 0 (Success)
 */
 
 int main(void)
