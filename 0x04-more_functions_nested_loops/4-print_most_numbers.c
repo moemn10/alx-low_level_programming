d770cee8fd92a457ec45aab7b739186ef1da4846
moemn10 <moemnkoko@gmail.com>
2023-03-24 01:34:25 -0700

    Moamen

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print most of numbers with
+ *		two number.
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_most_numbers(void);
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		if (n == 50 || n == 52)
+			n++;
+		_putchar(n);
+	_putchar('\n');
+}
