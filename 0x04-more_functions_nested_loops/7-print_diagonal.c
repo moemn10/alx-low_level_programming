ba9b8704a0b13de4e775c79d29668b79fbe34f0e
moemn10 <moemnkoko@gmail.com>
2023-03-24 15:11:57 -0700

    Moamen

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print slash in lines
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_diagonal(int n)
+{
+	int l, e;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{	for (e = 1; e <= n; e++)
+		{
+			for (l = 1; l<=e; l++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			
+		_putchar('\n');
+
+		}
+	}
+}
