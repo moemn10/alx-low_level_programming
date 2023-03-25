33a4fe28f8605c22efd234c433d892b490e21c5b
moemn10 <moemnkoko@gmail.com>
2023-03-24 17:18:53 -0700

    Moamen

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer number
+ *
+ * @n: the integer number we printed
+*/
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + 48);
+}
