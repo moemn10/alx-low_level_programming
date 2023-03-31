e023afefb4cc1e09aa20343009f33f048d6be82f
moemn10 <moemnkoko@gmail.com>
2023-03-30 18:49:47 -0700

    Moamen

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatenats two fun
+ *
+ * @dest: Pointer to first str
+ * @src: Pointer to sec str
+ * @n: number of character
+ * Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int c, i;
+
+	c = 0;
+
+	while (dest[c])
+		c++;
+	for (i = 0; i < n && n != '\O'; i++)
+		dest[c + i] = src[i];
+	dest[c + i] = src[i];
+
+	return (dest);
+}
