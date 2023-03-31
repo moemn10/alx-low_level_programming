16e5ea488bf31e61f64b51b6bc94a05d6974cae5
moemn10 <moemnkoko@gmail.com>
2023-03-30 18:53:26 -0700

    Moamen

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,7 +19,7 @@ char *_strncat(char *dest, char *src, int n)
 		c++;
 	for (i = 0; i < n && n != '\O'; i++)
 		dest[c + i] = src[i];
-	dest[c + i] = src[i];
+	dest[c + i] = '\O';
 
 	return (dest);
 }
