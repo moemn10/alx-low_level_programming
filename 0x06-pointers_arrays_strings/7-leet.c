e5af419f9c877f25f26e9ea13bfb81c0d4759206
moemn10 <moemnkoko@gmail.com>
2023-03-31 04:59:48 -0700

    Moamen

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,9 +1,27 @@
 #include "main.h"
 
 /**
+ * leet - encoder function
+ * @c: input value
+ *
+ * Return: cp
 */
 
-char *leet(char *)
+char *leet(char *c)
 {
-	
+	char *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+			if (*c == key[i] || *c == key[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
+		c++;
+	}
+	return (cp);
 }
