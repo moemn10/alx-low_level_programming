5509282cd5815c7a2d1bb410843cdfe5f92f7595
moemn10 <moemnkoko@gmail.com>
2023-03-30 18:33:37 -0700

    Moamen

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,11 +15,11 @@ char *_strcat(char *dest, char *src)
 	int c, c2;
 
 	c = 0;
-	while (dest[c]);
+	while (dest[c])
 		c++;
 	for (c2 = 0; src[c2]; c2++)
 		dest[c++] = src[c2];
 
-	return (dest);	
+	return (dest);
 
 }
