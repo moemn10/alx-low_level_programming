2edd2ef2c3ea9892a87a9395800d9b600a337f34
moemn10 <moemnkoko@gmail.com>
2023-04-08 20:06:44 -0700

    libmy

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if the input is digit or not.
+ *
+ * @c: input for digit
+ *
+ * Return: 1 if digit and 0 if not
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	return (1);
+	else
+	return (0);
+}
