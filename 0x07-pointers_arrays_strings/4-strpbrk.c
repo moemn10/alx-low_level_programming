513cbac39ace40758f7e86fc19cc55a225f39087
moemn10 <moemnkoko@gmail.com>
2023-04-08 19:44:18 -0700

    moamen

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - search
+ * @s: string
+ * @accept: string no match
+ * Return: pointer.
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int itr, jtr;
+	char *p;
+
+	itr = 0;
+	while (s[itr] != '\0')
+	{
+		jtr = 0;
+		while (accept[jtr] != '\0')
+		{
+			if (accept[jtr] == s[itr])
+			{
+				p = &s[itr];
+				return (p);
+			}
+			jtr++;
+		}
+		itr++;
+	}
+	return (0);
+}
