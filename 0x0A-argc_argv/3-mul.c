977522bf571c08e3b66f7ecffa10530ddebded79
moemn10 <moemnkoko@gmail.com>
2023-04-08 21:40:32 -0700

    moamne

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,12 +13,7 @@ int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+	i = d = n = len = f = digit = 0;
 	while (s[len] != '\0')
 		len++;
 	while (i < len && f == 0)
@@ -32,7 +27,7 @@ int _atoi(char *s)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > 9)
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
 		}
@@ -42,7 +37,6 @@ int _atoi(char *s)
 		return (0);
 
 	return (n);
-
 }
 
 /**
@@ -65,6 +59,7 @@ int main(int argc, char *argv[])
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
+
 	printf("%d\n", result);
 
 	return (0);
