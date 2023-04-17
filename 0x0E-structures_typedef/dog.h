5832a5c3dee5fe62c834c964d5ba701888a10ecc
moemn10 <moemnkoko@gmail.com>
2023-04-17 16:56:44 -0700

    moamen

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Descrption: just a long dog struct in a big
+ *
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
