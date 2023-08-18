b9e79a3520d4749e23dfbf45f421d720796d3171
moemn10 <moemnkoko@gmail.com>
2023-08-18 15:26:03 -0700

    moemn

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,22 +10,12 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
-
-	count = 0;
-
-	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
+	size_t nodes = 0;
+	while (h)
 	{
-		printf("%d\n", h->n);
-		count++;
+		nodes++;
+		printf("%d\n" , h->n);
 		h = h->next;
 	}
-
-	return (count);
+	return  (nodes);
 }
