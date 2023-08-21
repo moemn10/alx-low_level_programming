38f9d3c4d8965bc3c7d83031095b9149f7ca5f27
moemn10 <moemnkoko@gmail.com>
2023-08-21 13:03:48 -0700

    moemn

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/moemn10/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -P https://github.com/moemn10/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
